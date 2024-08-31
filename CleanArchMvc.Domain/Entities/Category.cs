31c8a44b126bf5cd19d79a8b3076f3dfc5678814
GenivaldoCarvalho <gezinho.souza321@gmail.com>
2024-08-30 21:14:17 -0300

    implementando camada Infraestrutura

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,47 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category : Entity
+        //sealed não deixa ser herdada
+    {
+       
+        public string Name { get; private  set; }
+
+        //criar um construtor - digita ctor
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+        public Category(int id, string name)
+        {
+            //criado na pasta Validation a excpetion
+            DomainExceptionValidation.When(id < 0, "Invalid ID value.");
+            Id = id;
+            ValidateDomain(name);
+        }
+        //atualização 
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public ICollection<Product> Products { get; set; }
+
+        public void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name.Name is required");
+            DomainExceptionValidation.When(name.Length < 3 ,
+                "Invalid name, too short, minimum 3 characters");
+            Name = name;
+        }
+
+
+    }
+}
---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,69 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Product : Entity
+    //sealed não deixa ser herdada
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private  set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+
+        //metodo sem Id
+        public Product(string name, string description, decimal price, int stock, string image) 
+        {
+            ValidateDomain(name, description, price, stock, image); 
+        }
+        //metodo com id
+        public Product( int id,string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
+            Id = id;
+            ValidateDomain(name,description, price, stock, image);            
+        }
+        //atualização
+        public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
+        {
+            ValidateDomain(name, description, price, stock, image);
+            CategoryId = categoryId;    
+        }
+
+
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Ivalid name.Name Name is required");
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description),
+                "Ivalid description.Name Name is required");
+            DomainExceptionValidation.When(description.Length < 5,
+                "Invalid name, minimo 05 caracteres");
+
+            DomainExceptionValidation.When(price < 0, "Invalid price value");
+
+            DomainExceptionValidation.When(stock < 0, "Invalid stock value");
+
+            DomainExceptionValidation.When(image?.Length > 250,
+                "Invalid image name, too long, maximum 250 characters");
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
+
+        //propriedade de navegação
+        public int CategoryId { get;  set; }
+        public Category Category {get; set; }
+    }
+}
